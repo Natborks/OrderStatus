3363742f844e3e37cde5b4b57a6486bfb8ffd39d
natborks <natborks@gmail.com>
2018-12-13 05:16:04 -0500

    Create layouts

---
+++ app/src/main/java/com/borketeykwaku3630/fe_216cs01003630/Summary.java
@@ -0,0 +1,15 @@
+//Nathanael Borketey-Kwaku
+
+package com.borketeykwaku3630.fe_216cs01003630;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Summary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+}
